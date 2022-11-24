78872e50d85d63fff4a44a54784adfc2f9bbf94e
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-24 21:23:54 +0300

    add Example_5 с затачей

--- Семинар_2/Example_5/Program.cs
+++ Семинар_2/Example_5/Program.cs
@@ -4,3 +4,13 @@
 //25, 5  ->  да
 //8,9  ->  нет
 
+Console.Clear();
+
+Console.Write("Введите первое число : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2*num2) Console.WriteLine($"Число {num1} является квадратом числа {num2}.");
+else if (num2 == num1*num1) Console.WriteLine($"Число {num2} является квадратом числа {num1}.");
+else Console.WriteLine($"Числа {num1} и {num2} не являются квадратами друг друга.");
