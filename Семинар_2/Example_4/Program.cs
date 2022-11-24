56c4fe7fceedeab851f6698bba4a88ad555efc9b
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-24 21:19:44 +0300

    add Example_4 с затачей

--- Семинар_2/Example_4/Program.cs
+++ Семинар_2/Example_4/Program.cs
@@ -2,3 +2,11 @@
 //14 -> нет 
 //46 -> нет 
 //161 -> да
+
+Console.Clear();
+
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num % 7 == 0 && num % 23 == 0 ? $"Число {num} кратно 7 и 23." 
+: $"Число {num} не кратно одновременно 7 и 23");
