edd1b4cc53bdebf446e8f46803f597c41871ce21
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-24 20:41:56 +0300

    add Example_2 с задачей

--- Семинар_2/Example_2/Program.cs
+++ Семинар_2/Example_2/Program.cs
@@ -2,3 +2,16 @@
 //456 -> 46
 //782 -> 72
 //918 -> 98
+
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine($"Сгенерировано случайное чило - {num}");
+
+int num1 = num / 100;
+int num2 = num % 10;
+
+int res = num1 * 10 + num2;
+
+Console.WriteLine($"Получилось двузначное число - {res}");
