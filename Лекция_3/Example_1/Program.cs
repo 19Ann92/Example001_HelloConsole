dcf293f1fdb66f7835fc4393e27549991487bdfd
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-29 21:01:52 +0300

    add Example 1 Добавила еще информации

--- Лекция_3/Example_1/Program.cs
+++ Лекция_3/Example_1/Program.cs
@@ -39,18 +39,43 @@ int year = Method3();
 
 //Вид 4 - Что то принимают и что то возвращают
 
+//string Method4(int count, string text)
+//{
+//    int i = 0;
+//    string result = String.Empty;
+//
+//    while (i < count)
+//    {
+//        result = result + text;
+//        i++;
+//  }
+//    return result;
+//}
+
+
 string Method4(int count, string text)
 {
-    int i = 0;
+
     string result = String.Empty;
 
-     while (i < count)
-     {
+    for (int i = 0; i < count; i++)
+    {
         result = result + text;
-        i++;
-     }
-     return result;
+    }
+    return result;
 }
 
-string res = Method4(10, "z");
-Console.WriteLine(res);
+//string res = Method4(10, "z");
+//Console.WriteLine(res);
+
+
+//Цикл в цикле
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j}={i * j}");
+    }
+    Console.WriteLine();
+}
