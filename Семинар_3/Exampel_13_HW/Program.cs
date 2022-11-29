65ef23c0aa8a5334ffa80691c49355b54514843c
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-29 22:18:08 +0300

    Exampel 13 HW

---
+++ Семинар_3/Exampel_13_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100)
+{
+Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+while (num > 999)
+{
+num = num / 10;
+}
+Console.WriteLine(num % 10);
+}
