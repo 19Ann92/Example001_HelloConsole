9bbb3124d368de888ebd2757f1591379a0eeb5f0
Anna Eliseeva <Panterka6868@mail.ru>
2022-12-01 22:05:37 +0300

    add Example 4

---
+++ Семинар_4/Example_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+
+int[] FillingArray(int n){
+    Random rnd = new Random();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = rnd.Next(0,2);
+    }
+    return arr;
+}
+
+int[] arr = FillingArray(8);
+Console.WriteLine(String.Join(" ", arr));
+
+
+
+
