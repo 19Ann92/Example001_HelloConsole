d9c0c4ebe9be5b55af607fcfc97b1fc3fae39b0b
Anna Eliseeva <Panterka6868@mail.ru>
2022-12-12 22:07:09 +0300

    add Example4

---
+++ Семинар_7/Example4/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 49 2
+// 5 9 2 4
+// 64 4 4 4
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeMassive(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                inputArray[i, j] = inputArray[i, j] * inputArray[i, j];
+            }
+        }
+    }
+    return inputArray;
+}
+
+PrintArray(array);
+Console.WriteLine();
+ChangeMassive(array);
+Console.WriteLine();
+PrintArray(array);
